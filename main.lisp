c795c3ae72c3411b78011fbdf801b6472994b107
Burton Samograd <burton.samograd@gmail.com>
2015-03-09 09:26:00 -0600

    Re-indentation and adding back a spawn with pop terminal io for early debgging.

--- main.lisp
+++ main.lisp
@@ -73,18 +73,21 @@
                                                  left right)
                                               (- (mezzano.gui.compositor:height window) 
                                                  top bottom)))
-        (switch-to-buffer (get-buffer-create "*scratch*"))
+    (switch-to-buffer (get-buffer-create "*scratch*"))
+    (let ((buffer (get-buffer-create "*Messages*")))
+      (insert buffer "Welcome to the Mezzano EDitor. Happy Hacking!")
+      (insert buffer #\Newline))
         (ignore-errors
-        (when initial-file
-          (find-file initial-file)))
-        (catch 'quit
-          (loop
-            (handler-case
-              (editor-loop)
-              (error (c)
-              (ignore-errors
-                (format t "Editor error: ~A~%" c)
-                (setf (pending-redisplay *editor*) t))))))))))))
+          (when initial-file
+            (find-file initial-file)))
+          (catch 'quit
+            (loop
+              (handler-case
+                (editor-loop)
+                (error (c)
+                (ignore-errors
+                  (format t "Editor error: ~A~%" c)
+                  (setf (pending-redisplay *editor*) t))))))))))))
 
 (defun spawn (&key width height initial-file)
   (mezzano.supervisor:make-thread
@@ -98,3 +101,17 @@
                         (*trace-output* ,(make-synonym-stream '*terminal-io*))
                         (*debug-io* ,(make-synonym-stream '*terminal-io*))
                         (*query-io* ,(make-synonym-stream '*terminal-io*)))))
+
+;; Keep this around for a bit for early debugging
+#+() (defun spawn (&key width height initial-file)
+  (mezzano.supervisor:make-thread (lambda () (editor-main width height initial-file))
+                                  :name "Editor"
+                                  :initial-bindings `((*terminal-io* ,(make-instance 'mezzano.gui.popup-io-stream:popup-io-stream
+                                                                                     :title "Editor console"))
+                                                      (*standard-input* ,(make-synonym-stream '*terminal-io*))
+                                                      (*standard-output* ,(make-synonym-stream '*terminal-io*))
+                                                      (*error-output* ,(make-synonym-stream '*terminal-io*))
+                                                      (*trace-output* ,(make-synonym-stream '*terminal-io*))
+                                                      (*debug-io* ,(make-synonym-stream '*terminal-io*))
+                                                      (*query-io* ,(make-synonym-stream '*terminal-io*)))))
+
