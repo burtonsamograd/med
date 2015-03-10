cd7d4f6819f436238993310ab0dca6061838900a
Burton Samograd <burton.samograd@gmail.com>
2015-03-10 11:47:03 -0600

    Fix buffer-stream, which was causing massive slowdowns due to errors because of missing package prefix on col-index-of.

--- buffer-stream.lisp
+++ buffer-stream.lisp
@@ -4,7 +4,7 @@
   ((buffer :initarg :buffer-name :reader buffer-stream-buffer-name)))
 
 (defmethod sys.gray::stream-line-column ((stream buffer-stream))
-  (col-index-of stream))
+  (sys.gray::col-index-of stream))
 
 (defmethod sys.gray::stream-write-char ((stream buffer-stream) char)
   (let ((buffer (get-buffer-create (buffer-stream-buffer-name stream))))
