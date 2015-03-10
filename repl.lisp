ebffc6cc8b9c7906705d21abb00f61283ee9c4aa
Burton Samograd <burton.samograd@gmail.com>
2015-03-09 23:07:44 -0600

    More repl.

--- repl.lisp
+++ repl.lisp
@@ -1,7 +1,21 @@
 (in-package :med)
 
+(defvar *repl-key-map*)
+
 (defun start-repl ()
+  (initialize-repl-key-map)repl.lisp
+
   (let ((buffer (get-buffer "*repl*")))
     (unless buffer
-      (make-instance 'buffer :name "*repl*"))
+      (setf buffer (make-instance 'buffer
+                                  :key-map *repl-key-map*))
+      (setf (buffer-property buffer 'name) "*repl*")
+      (push buffer (buffer-list)))
   (switch-to-buffer buffer)))
+
+(defun repl-finish-input-command ())
+
+(defun initialize-repl-key-map ()
+  (setf *repl-key-map* (make-hash-table))
+  (set-key #\C-M 'repl-finish-input-command *repl-key-map*)
+)
